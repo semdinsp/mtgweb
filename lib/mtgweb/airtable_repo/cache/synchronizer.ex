43ef0c36334ab469aa88f5f74b7b7c06bdae82a3
Scott Sproule <scott.sproule@ficonab.com>
2022-10-01 01:07:27 -0400

    updates for temr page... need sot fix doark more on pricing

--- lib/mtgweb/airtable_repo/cache/synchronizer.ex
+++ lib/mtgweb/airtable_repo/cache/synchronizer.ex
@@ -9,7 +9,7 @@ defmodule Mtgweb.AirtableRepo.Cache.Synchronizer do
 
   use GenServer
 
-  @refresh_time :timer.seconds(60)  # scott was 1
+  @refresh_time :timer.seconds(20)  # scott was 1
 
   def start_link(opts) do
     GenServer.start_link(__MODULE__, opts)
