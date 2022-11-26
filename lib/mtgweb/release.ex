39e7b78758bac9b051426852164b46791b612c6c
Scott Sproule <scott.sproule@ficonab.com>
2022-11-25 22:55:02 -0800

    rying to fix deploy

--- lib/mtgweb/release.ex
+++ lib/mtgweb/release.ex
@@ -8,18 +8,18 @@ defmodule Mtgweb.Release do
   def migrate do
     load_app()
 
-    for repo <- repos() do
-     # {:ok, _, _} = Ecto.Migrator.with_repo(repo, &Ecto.Migrator.run(&1, :up, all: true))
-    end
+    #for repo <- repos() do
+    #  {:ok, _, _} = Ecto.Migrator.with_repo(repo, &Ecto.Migrator.run(&1, :up, all: true))
+   # end
   end
 
-  def rollback(repo, version) do
+  def rollback(_repo, _version) do
     load_app()
    # {:ok, _, _} = Ecto.Migrator.with_repo(repo, &Ecto.Migrator.run(&1, :down, to: version))
   end
 
   defp repos do
-    Application.fetch_env!(@app, :ecto_repos)
+   # Application.fetch_env!(@app, :ecto_repos)
   end
 
   defp load_app do
