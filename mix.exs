de3b47687146bb012826165c2e5c40299c42eac9
Scott Sproule <scott.sproule@ficonab.com>
2026-06-08 13:23:02 -0400

    update for latest elixir versions

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Mtgweb.MixProject do
     [
       app: :mtgweb,
       version: "0.1.0",
-      elixir: "~> 1.20",
+      elixir: "~> 1.19",
       elixirc_paths: elixirc_paths(Mix.env()),
       compilers: [] ++ Mix.compilers(),
       start_permanent: Mix.env() == :prod,
